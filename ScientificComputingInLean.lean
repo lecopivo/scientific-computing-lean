657e20f8e4c93595b9975f481dc1c83883613e53
lecopivo <skrivantomas@seznam.cz>
2024-09-18 03:38:32 -0400

    clean up start page

--- ScientificComputingInLean.lean
+++ ScientificComputingInLean.lean
@@ -16,51 +16,7 @@ set_option pp.rawOnError true
 authors := ["Tomáš Skřivan"]
 %%%
 
-The Plan 🙂
-
-1. Arrays
-
-2. Differentiations
-
-3. Equations, optimization and their approximation
-  - inverse function
-  - general approximation
-  - nonlinear equations - newton solver    
-  - linear equations - todo: add eigen bindings
-  - minimization - min function
-  - differentiation equations and minimizations
-    - differentiating inverse function
-    - implicit differentiation
-    - differentiating minimization problem
-  
-4. Differential equations
-  - odesolve
-  - numerical methods
-  - differentiating ode solutions
-
-5. Geometry
-  - shapes
-  - level sets and SDFs
-  - bounding balls and squares
-
-6. probabilistic programming
-  - samplers
-  - differentiating probabilistic programs
-  - differentiating parametric discontinuities
-  - random programs with traces
-
-7. Tactic overview
-  - fun\_trans
-  - fun\_prop
-  - infer\_var
-  - data\_synth
-  - lsimp
-  - rsimp
-  - optimize_index_access
-  
-
-8. Examples
-
+This is **work in progress** book on scientific computing in Lean. It also serves as reference manual for the library [SciLean](https://github.com/lecopivo/SciLean).
 
 {include ScientificComputingInLean.Intro}
 
