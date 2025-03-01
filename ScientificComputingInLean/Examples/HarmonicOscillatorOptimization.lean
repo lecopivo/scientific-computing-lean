86b59a92a8d93baee46b78e347ecbe306a5a186a
lecopivo <skrivantomas@seznam.cz>
2025-03-01 16:08:48 -0500

    update on harmonic oscillator optimization example

--- ScientificComputingInLean/Examples/HarmonicOscillatorOptimization.lean
+++ ScientificComputingInLean/Examples/HarmonicOscillatorOptimization.lean
@@ -15,18 +15,6 @@ open SciLean
 
 set_default_scalar Float
 
-
-open Lean Parser Term in
-macro "argmin" xs:funBinder* ", " b:term : term => do
-  `(Function.argmin ↿fun $xs* => $b)
-
-theorem solve_eq_argmin_norm2
-    (R : Type*) [RealScalar R]
-    {X : Type*} [NormedAddCommGroup X] [AdjointSpace R X] [CompleteSpace X]
-    {Y : Type*} [NormedAddCommGroup Y] [AdjointSpace R Y] [CompleteSpace Y]
-    {f : X → Y} {y : Y} (hf : HasUniqueSolution (fun x => f x = y)) :
-    (solve x, f x = y) = argmin x, ‖f x - y‖₂²[R] := sorry_proof
-
 theorem revFDeriv_eq_fwdFDeriv
     {R : Type*} [RealScalar R]
     {f : R → R} :
@@ -38,72 +26,103 @@ theorem revFDeriv_eq_fwdFDeriv
 
 open Optimjl
 
--- not sure how to define this yet
-opaque Options.filter {R : Type} [RealScalar R] : Filter (Options R) := default
-
-theorem argmin_eq_limit_optimize
-    {R : Type} [RealScalar R] [ToString R]
-    {X : Type} [NormedAddCommGroup X] [AdjointSpace R X] [CompleteSpace X] [ToString X]
-    {Method : Type*} {State : outParam Type} [AbstractOptimizer Method State R X]
-    (method : Method) (x₀ : X)
-    {f : X → R} :
-    (argmin x, f x)
-    =
-    limit opts ∈ Options.filter (R:=R),
-      let f' := holdLet <| revFDeriv R f
-      let r := optimize' {f:=f,f':=f',hf:=sorry_proof} (AbstractOptimizer.setOptions X method opts) x₀
-      r.minimizer := sorry_proof
-
-
-@[fun_prop]
-theorem holdLet.arg_a.Differentiable_rule
-  {𝕜} [RCLike 𝕜] {X} [NormedAddCommGroup X] [NormedSpace 𝕜 X] :
-  IsContinuousLinearMap 𝕜 fun x : X => holdLet x := by simp[holdLet]; fun_prop
-
-@[fun_prop]
-theorem holdLet.arg_a1.Differentiable_rule
-  {𝕜} [RCLike 𝕜]
-  {X} [NormedAddCommGroup X] [NormedSpace 𝕜 X]
-  {Y} [NormedAddCommGroup Y] [NormedSpace 𝕜 Y]
-  (f : X → Y) (hf : Differentiable 𝕜 f):
-  Differentiable 𝕜 (holdLet f) := by simp[holdLet,hf]
-
-@[fun_prop]
-theorem holdLet.arg_a1.IsContinusousLinearMap_rule
-  {𝕜} [RCLike 𝕜]
-  {X} [NormedAddCommGroup X] [NormedSpace 𝕜 X]
-  {Y} [NormedAddCommGroup Y] [NormedSpace 𝕜 Y]
-  (f : X → Y) (hf : IsContinuousLinearMap 𝕜 f):
-  IsContinuousLinearMap 𝕜 (holdLet f) := by simp[holdLet,hf]
-
-@[fun_trans]
-theorem holdLet.arg_a1.fwdFDeriv_rule
-  {𝕜} [RCLike 𝕜]
-  {X} [NormedAddCommGroup X] [NormedSpace 𝕜 X]
-  {Y} [NormedAddCommGroup Y] [NormedSpace 𝕜 Y]
-  (f : X → Y) :
-  fwdFDeriv 𝕜 (holdLet f) = holdLet (fwdFDeriv 𝕜 f) := by rfl
-
-@[simp, simp_core]
-theorem holdLet_apply {α β : Type*} (f : α → β) (x : α) : holdLet f x = f x := rfl
-
-
 #doc (Manual) "🚧 Harmonic Oscillator Optimization" =>
 
 ```lean (show := false)
 namespace HarOscOpt
 ```
 
-```lean
-def H (m k : Float) (x p : Float) := (1/(2*m)) * p^2 + k/2 * x^2
+# Problem Statement
 
-approx solver (m T X k₀ : Float) :=
-  let y := holdLet <| fun (k : Float) =>
-    odeSolve (t₀ := 0) (t:=T) (x₀:=(X,0))
+In this example, we will demonstrate how to find a parameter of differential equation such that the solution satisfies particular property. Consider this practical problem. We are designing a new musical instrument, we have already decided on its particular shape. We want to hit a particular note and we will do that by finding the right material. To do this we model the musical instrument with harmonic oscillator and we will find the right stifness value that gives us solution with desired frequency.
+
+Of course that in reality we would optimize for the shape rather than for the material but this would yield much harder mathematical problem. Therefore we do it the other way around which is much easier, serves as demonstration on how to use SciLean and we can easily check the numrical answer to the analytical solution.
+
+
+The harmonic oscillator is governed by the following differential equation
+```latex
+m \ddot x = - k x
+```
+because we will try to find the appropricate `k` we will denote \\(x(k,t)\\) the solution of this equation as a function of time \\(t\\) and stiffness \\(k\\).
+
+How can we express the requirement that the solution should hit a particular frequencty \\(\omega\\)? One way to state this requirement is that the solution should return to the same position after time \\(T\\). We want to find stiffness \\(k\\) such that
+```latex
+x(k,T) = x(k,0)
+```
+where the time \\(T\\) is related to the given frequency by
+```latex
+T = \frac{2 \pi}{\omega}
+```
+
+(You might have noticed that this argument contains a flaw which we kindly ask you to ignore for the moment as we will discuss it at the end.)
+
+
+# Lean Specification
+
+Lets formulate this in Lean now. In the previous example 'Harmonic Oscillator' we have shows how to specify harmonic oscillator using its energy
+```lean
+def H (m k x p : Float) := (1/(2*m)) * p^2 + k/2 * x^2
+```
+The solution of the corresponding differential equation is symbolically represented using the function {name}`odeSolve`
+```lean
+noncomputable
+def solution (m k x₀ p₀ t : Float) : Float×Float :=
+  odeSolve (t₀ := 0) (x₀ := (x₀,p₀)) (t := t)
+    (fun (t : Float) (x,p) =>
+        ( ∇ (p':=p), H m k x  p',
+         -∇ (x':=x), H m k x' p))
+```
+The expression `solution m k x₀ p₀ t` is the position and momentum at time `t` of harmonic oscillator with mass `m`, stiffness `k` starting at time `0` at position `x₀` and momentum `p₀`.
+
+Notice that we had to mark the function {name}`solution` with `noncomputable` as it is purely symbolic as it can't be executed because it uses the function {name}`odeSolve`.
+
+To express that we are looking for the stiffness satisfying a particular property we can use the notation `solve x, f x = 0` which will symbolically return the solution `x` of the equation `f x = 0`.
+```lean
+noncomputable
+def optimalStiffness (m x₀ ω : Float) : Float :=
+  let x := fun k t =>
+    let (x,p) := solution m k x₀ 0 t
+    x
+  let T := 2*π/ω
+  solve k, x k T = x k 0
+```
+The notation `solve x, f x = 0` uses the noncomputable function {name}`solveFun` which returns a term that satisfies a particular property. This is why we have to mark {name}`optimalStiffness` as noncomputable.
+
+Our goal is to have an executable function that can approximatelly compute the symbolic function {name}`optimalStiffness`.
+
+We can therefore define function `findStiffness` that is an approximation to the specification. In the next section we will show how replace `skip` with a sequence of tactics that will turn the specification into executable approximation and allows us to remove the `noncomputable` annotation from this fuction.
+```lean (keep:=false)
+noncomputable
+approx findStiffness (m x₀ ω : Float) :=
+  let T := 2*π/ω
+  let y := fun (k : Float) =>
+    odeSolve (t₀ := 0) (t:=T) (x₀:=(x₀,0))
       (fun (t : Float) (x,p) =>
         ( ∇ (p':=p), H m k x  p',
          -∇ (x':=x), H m k x' p))
-  solve k, (y k).1 = X
+  solve k, (y k).1 = x₀
+by
+  skip
+```
+
+# Turning Specification into Implementation
+
+
+Unfortunatelly the current documentation tool we are using does not allow mixing a proof with text so we have to write down everything with only brief comments and only then we will go over each tactic and explain what it does.
+
+The are two little differences in the initial specification that are mainly of technical nature. First, we had to add the function {name}`holdLet` which is just an identity function and should be ignored. Its purpose is to prevent inlining of `y` by the tactic `autodiff`. The se
+
+Please go ahead and have a brief look at every step. The short comments should give you rought idea what each tactic is doing and by clicking on each command will display the current state.
+
+```lean
+approx findStiffness (m x₀ ω k₀ : Float) :=
+  let T := 2*π/ω
+  let y := holdLet <| fun (k : Float) =>
+    odeSolve (t₀ := 0) (t:=T) (x₀:=(x₀,0))
+      (fun (t : Float) (x,p) =>
+        ( ∇ (p':=p), H m k x  p',
+         -∇ (x':=x), H m k x' p))
+  solve k, (y k).1 = x₀
 by
   conv =>
     -- focus on the specification
@@ -113,8 +132,8 @@ by
     unfold H; autodiff
 
     conv =>
-      -- focus on solve k, (y k).1 = X
-      enter[y]
+      -- focus on solve k, (y k).1 = x₀
+      enter[T,y]
 
       -- reformulate as minimization problem
       rw[solve_eq_argmin_norm2 Float (by sorry_proof)]
@@ -125,8 +144,6 @@ by
           (method := (default : LBFGS Float 1))]
 
   -- consume limit by `Approx`
-  -- approx limit is not respecting leading let binding!
-  -- I thing this is because of the final apply `Approx.limit _ _`
   approx_limit opts sorry_proof
 
   conv =>
@@ -147,19 +164,15 @@ by
   -- choose the same number of steps for both ODE solvers
   -- and consume the corresponding limin in `Approx`
   approx_limit steps sorry_proof
-
-```
-
-
-```lean
-#eval solver (m:=1) (T:=1) (X:=1) (k₀:=60)
-        ({show_trace := true, result_trace := true},200,())
 ```
 
 ```lean
-#eval solver (m:=1) (T:=1) (X:=1) (k₀:=60)
-        ({x_abstol := 1e-16, g_abstol := 0, init_alpha := 10,
-          show_trace := true, result_trace := true},200,())
+#eval (2*π)^2
+```
+
+```lean
+#eval findStiffness (m:=1) (ω:=2*π) (x₀:=1) (k₀:=10)
+        ({x_abstol := 1e-16, g_abstol := 0, show_trace := true, result_trace := true},200,())
 ```
 
 ```lean
